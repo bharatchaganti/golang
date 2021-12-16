6138b8744ffc0a8885e09821a09163c439c61cea
Bharat Chowdary Chaganti <bharatchagantivs@gmail.com>
2021-12-16 12:21:51 -0600

    package commit

---
+++ CalArea/go.mod
@@ -0,0 +1,3 @@
+module github.com/bharatchaganti/area
+
+go 1.17
---
+++ CalArea/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bharatchaganti/area"
+)
+
+func main() {
+	fmt.Println(area.CalculateArea(10, 20))
+}
